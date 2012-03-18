81a55ee8892a5f8fef78dda8f0205374209c821f
Tomás Guisasola <tomasguisasola@gmail.com>
2012-03-18 20:32:49 -0300

    Correção de bug no cálculo do bônus de ataque das armas mágicas. Acréscimo de lâmina da canção pungente.

--- Armas.lua
+++ Armas.lua
@@ -5,9 +5,10 @@ local set = require"DnD.Set"
 local function magica (nome, bonus)
 	local arma = ArmasBasicas[nome]
 	local m = {
+		nome = arma.nome.." Mágica",
 		basica = nome,
-		ataque = bonus,
-		dano = bonus,
+		proficiencia = bonus + arma.proficiencia,
+		dano = soma_dano ({}, arma.dano, bonus),
 		decisivo = bonus.."d6",
 	}
 	return setmetatable (m, { __index = arma, })
@@ -18,8 +19,8 @@ local function algida (nome, bonus)
 	local m = {
 		nome = arma.nome.." Álgida",
 		basica = nome,
-		ataque = bonus,
-		dano = bonus,
+		proficiencia = bonus + arma.proficiencia,
+		dano = soma_dano ({}, arma.dano, bonus),
 		decisivo = bonus.."d6",
 		poder = {
 			nome = "Arma Álgida",
@@ -32,31 +33,52 @@ local function algida (nome, bonus)
 	return setmetatable (m, { __index = arma, })
 end
 
-local function inescapavel (nome, bonus)
+local function inescapavel (nome, bonus) -- AA 72
 	local arma = ArmasBasicas[nome]
 	local m = {
 		nome = arma.nome.." Inescapável",
 		basica = nome,
-		ataque = bonus,
-		dano = bonus,
+		proficiencia = bonus + arma.proficiencia,
+		dano = soma_dano ({}, arma.dano, bonus),
 		decisivo = bonus.."d6",
 		efeito = "Efeito: sempre que fracassar em um ataque com esta arma, recebe +1 (até o bônus\n    máximo da arma) no próximo ataque contra o mesmo alvo.\n    O bônus se encerra se acertar o alvo ou trocar de alvo.",
 	}
 	return setmetatable (m, { __index = arma, })
 end
 
-local function rapida (nome, bonus)
+local function da_cancao_pungente (nome, bonus) -- LJ2 204
+	local arma = ArmasBasicas[nome]
+	local m = {
+		nome = arma.nome.." da Canção Pungente",
+		basica = nome,
+		proficiencia = bonus + arma.proficiencia,
+		dano = soma_dano ({}, arma.dano, bonus),
+		decisivo = bonus.."d8",
+		efeito = "Efeito: serve como implemento de bardo e de trilha exemplar de bardo.",
+		poder = {
+			nome = "Lâmina da Canção Pungente",
+			uso = "Di",
+			acao = "livre",
+			origem = {},
+			efeito = "Quando atingir um inimigo com um poder trovejante usando esta lâmina,\n    os inimigos a até 2 quadrados do alvo ficam pasmos até o FdPT.",
+		},
+	}
+	return setmetatable (m, { __index = arma, })
+end
+
+local function rapida (nome, bonus) -- AA 76
 	local arma = ArmasBasicas[nome]
 	local m = {
 		nome = arma.nome.." Rápida",
 		basica = nome,
-		proficiencia = bonus+arma.proficiencia,
+		proficiencia = bonus + arma.proficiencia,
 		dano = soma_dano ({}, arma.dano, bonus),
 		decisivo = bonus.."d6",
 		poder = {
 			nome = "Arma Rápida",
-			uso = "Diário",
-			origem = set("marcial"),
+			uso = "Di",
+			acao = "livre",
+			origem = {},
 			efeito = "Efeito: quando acertar inimigo, realiza At.Básico (livre) contra inimigo qualquer.",
 		},
 	}
@@ -69,6 +91,7 @@ local armas = {
 	azagaia_magica_1 = magica ("azagaia", 1),
 	espada_longa_magica_1 = magica ("espada_longa", 1),
 	espada_longa_algida_3 = algida ("espada_longa", 1),
+	espada_longa_da_cancao_pungente_3 = da_cancao_pungente ("espada_longa", 1),
 	espada_grande_algida_3 = algida ("espada_grande", 1),
 
 	manopla_shuriken_rapida_3 = rapida ("manopla_shuriken", 1),
