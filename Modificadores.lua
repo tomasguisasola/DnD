fc280662444289445157ecd28fd84afa0f4eeb69
Tomás Guisasola <tomasguisasola@gmail.com>
2012-04-10 02:13:03 -0300

    Correção nos cálculos de ataque e dano de poderes que podem ser corpo-a-corpo ou à distância.

--- Modificadores.lua
+++ Modificadores.lua
@@ -1,4 +1,5 @@
 local soma_dano = require"DnD.Soma".soma
+local armas = require"DnD.Armas"
 
 local function modificador_atributo(atributo)
 	return "mod_"..atributo:sub(1,3):lower()
@@ -26,25 +27,57 @@ return {
 	carisma = mod"car",
 
 	dobra_21 = function(dado, atributo, nome)
-		local mod_atr = modificador_atributo(atributo)
 		local dobro_dado = dado:gsub("^1", "2")
 		if dobro_dado == dado then
 			dobro_dado = "2"..dado
 			dado = "1"..dado
 		end
-		return function(self)
-			if self.nivel >= 21 then
-				return soma_dano(self, dobro_dado, self[mod_atr], nome)
+		return function(self, dano, poder_arma)
+			local atr
+			if atributo:match"/" then -- forca/destreza
+				local poder, nome_arma = poder_arma:match"^([^+]*)%+(.*)$"
+				local arma = armas[nome_arma]
+				if arma.tipo:match"dist" then
+					atr = self.mod_des
+				else
+					atr = self.mod_for
+				end
 			else
-				return soma_dano(self, dado, self[mod_atr], nome)
+				atr = self[modificador_atributo(atributo)]
+			end
+			if self.nivel >= 21 then
+				return soma_dano(self, dobro_dado, atr, nome)
+			else
+				return soma_dano(self, dado, atr, nome)
 			end
 		end
 	end,
 
 	dado_mod = function(dado, atributo, nome)
-		local mod_atr = modificador_atributo(atributo)
-		return function(self, ...)
-			return soma_dano(self, dado, self[mod_atr], nome)
+		return function(self, valor, poder_arma)
+			local atr
+			if atributo:match"/" then -- forca/destreza
+				local poder, nome_arma = poder_arma:match"^([^+]*)%+(.*)$"
+				local arma = armas[nome_arma]
+				if arma.tipo:match"dist" then
+					atr = self.mod_des
+				else
+					atr = self.mod_for
+				end
+			else
+				atr = self[modificador_atributo(atributo)]
+			end
+			return soma_dano(self, dado, atr, nome)
+		end
+	end,
+
+	forca_ou_destreza = function(self, ataque, poder_arma)
+		local poder, nome_arma = poder_arma:match"^([^+]*)%+(.*)$"
+		local arma = armas[nome_arma]
+		if arma.tipo:match"dist" then
+			return soma_dano(self, self.mod_des, ataque, poder_arma)
+		else
+			return soma_dano(self, self.mod_for, ataque, poder_arma)
 		end
 	end,
 }
