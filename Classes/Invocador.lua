37735f34a33e2e28d085bbb49287f10d72fe1b10
Tomas Guisasola <tomasguisasola@gmail.com>
2012-12-09 20:23:20 -0200

    Acréscimo de poder Sem Limite de Invocador.

--- Classes/Invocador.lua
+++ Classes/Invocador.lua
@@ -1,5 +1,6 @@
 local set = require"DnD.Set"
 local tipos_armas = require"DnD.TiposArmas"
+local mod = require"DnD.Modificadores"
 
 return {
 	nome = "Invocador",
@@ -28,6 +29,20 @@ return {
 	},
 	poderes = {
 ------- Poderes Sem Limite nível 1 ---------------------------------------------
+		luz_vingadora = {
+			nome = "Luz Vingadora",
+			uso = "SL",
+			acao = "padrão",
+			origem = set("divino", "implemento", "radiante"),
+			tipo_ataque = "corpo",
+			alvo = "uma criatura",
+			ataque = mod.sabedoria,
+			defesa = "Fort",
+			dano = mod.dobra_21 ("1d10", "sabedoria", "Luz Vingadora"),
+			efeito = function(self)
+				return "Sucesso: se um aliado do invocador adjacente ao alvo estiver sangrando,\n    o ataque causa +"..self.mod_con.." de dano radiante adicional."
+			end,
+		},
 ------- Poderes por Encontro nível 1 -------------------------------------------
 ------- Poderes Diários nível 1 ------------------------------------------------
 ------- Poderes Utilitários nível 2 --------------------------------------------
