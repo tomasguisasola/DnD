7c751c9c3ef14129eeb929548860a6dc51bf1dc7
Tomas Guisasola <tomasguisasola@gmail.com>
2013-07-15 16:04:19 -0300

    Cadastro de nomes dos implementos.

--- Implementos.lua
+++ Implementos.lua
@@ -6,6 +6,7 @@ local implemento_basico = require"DnD.Implemento_Basico"
 
 return {
 	bastao_magico_1 = {
+		nome = "Bastão Mágico",
 		tipo = "bastao",
 		ataque = implemento_basico("bastao", 1),
 		dano = implemento_basico("bastao", 1),
@@ -13,6 +14,7 @@ return {
 		preco = 360,
 	},
 	bastao_da_corrupcao_3 = {
+		nome = "Bastão da Corrupção",
 		tipo = "bastao",
 		ataque = implemento_basico("bastao", 1),
 		dano = implemento_basico("bastao", 1),
@@ -20,6 +22,7 @@ return {
 		preco = 640,
 	},
 	cajado_das_tempestades_5 = {
+		nome = "Cajado das Tempestades",
 		tipo = "cajado",
 		ataque = implemento_basico("cajado", 1),
 		dano = implemento_basico("cajado", 1),
@@ -37,6 +40,7 @@ return {
 		},
 	},
 	cajado_defensivo_2 = {
+		nome = "Cajado Defensivo",
 		tipo = "cajado",
 		ataque = implemento_basico("cajado", 1),
 		dano = implemento_basico("cajado", 1),
@@ -52,6 +56,7 @@ return {
 		end,
 	},
 	cajado_defensivo_7 = {
+		nome = "Cajado Defensivo",
 		tipo = "cajado",
 		ataque = implemento_basico("cajado", 2),
 		dano = implemento_basico("cajado", 2),
@@ -67,30 +72,35 @@ return {
 		end,
 	},
 	cajado_magico_1 = {
+		nome = "Cajado Mágico",
 		tipo = "cajado",
 		ataque = implemento_basico("cajado", 1),
 		dano = implemento_basico("cajado", 1),
 		decisivo = implemento_basico("cajado", "+1d6"),
 	},
 	cajado_magico_6 = {
+		nome = "Cajado Mágico",
 		tipo = "cajado",
 		ataque = implemento_basico("cajado", 2),
 		dano = implemento_basico("cajado", 2),
 		decisivo = implemento_basico("cajado", "+2d6"),
 	},
 	lamina_da_cancao_pungente_3 = { -- LJ2 204
+		nome = "Lâmina da Canção Pungente",
 		tipo = "lamina_da_cancao",
 		ataque = implemento_basico("lamina_da_cancao", 1),
 		dano = implemento_basico("lamina_da_cancao", 1),
 		decisivo = implemento_basico("lamina_da_cancao", "+1d6"),
 	},
 	simbolo_da_vida_2 = {
+		nome = "Símbolo da Vida",
 		tipo = "simbolo_sagrado",
 		ataque = implemento_basico("simbolo_sagrado", 1),
 		dano = implemento_basico("simbolo_sagrado", 1),
 		decisivo = implemento_basico("simbolo_sagrado", "+1d6"),
 	},
 	simbolo_da_esperanca_3 = {
+		nome = "Símbolo da Esperança",
 		tipo = "simbolo_sagrado",
 		ataque = implemento_basico("simbolo_sagrado", 1),
 		dano = implemento_basico("simbolo_sagrado", 1),
@@ -98,18 +108,21 @@ return {
 		preco = 680,
 	},
 	simbolo_da_batalha_5 = {
+		nome = "Símbolo da Batalha",
 		tipo = "simbolo_sagrado",
 		ataque = implemento_basico("simbolo_sagrado", 1),
 		dano = implemento_basico("simbolo_sagrado", 1),
 		decisivo = implemento_basico("simbolo_sagrado", "+1d8"),
 	},
 	totem_magico_1 = {
+		nome = "Totem Mágico",
 		tipo = "totem",
 		ataque = implemento_basico("totem", 1),
 		dano = implemento_basico("totem", 1),
 		decisivo = implemento_basico("totem", "+1d6"),
 	},
 	totem_do_crescimento_de_verao_3 = {
+		nome = "Totem do Crescimento do Verão",
 		tipo = "totem",
 		ataque = implemento_basico("totem", 1),
 		dano = implemento_basico("totem", 1),
@@ -121,6 +134,7 @@ return {
 		},
 	},
 	totem_da_renovacao_da_primavera_5 = {
+		nome = "Totem da Renovação da Primavera",
 		tipo = "totem",
 		ataque = implemento_basico("totem", 1),
 		dano = implemento_basico("totem", 1),
@@ -137,6 +151,7 @@ return {
 		},
 	},
 	varinha_magica_1 = {
+		nome = "Varinha Mágica",
 		tipo = "varinha",
 		ataque = implemento_basico("varinha", 1),
 		dano = implemento_basico("varinha", 1),
